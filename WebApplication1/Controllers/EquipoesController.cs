784e8ff6dc55e8f2f9fbdbd6a0b2393bc7bab051
Ianpro2000 <ian.proano@udla.edu.ec>
2022-02-02 08:00:29 -0500

    ya

---
+++ WebApplication1/Controllers/EquipoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EquipoesController : Controller
+    {
+        private bdVideoExplicativoEntitiesEntities db = new bdVideoExplicativoEntitiesEntities();
+
+        // GET: Equipoes
+        public ActionResult Index()
+        {
+            return View(db.Equipo.ToList());
+        }
+
+        // GET: Equipoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipo equipo = db.Equipo.Find(id);
+            if (equipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipo);
+        }
+
+        // GET: Equipoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Equipoes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdEquipo,NombreEquipo")] Equipo equipo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Equipo.Add(equipo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(equipo);
+        }
+
+        // GET: Equipoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipo equipo = db.Equipo.Find(id);
+            if (equipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipo);
+        }
+
+        // POST: Equipoes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdEquipo,NombreEquipo")] Equipo equipo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(equipo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(equipo);
+        }
+
+        // GET: Equipoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipo equipo = db.Equipo.Find(id);
+            if (equipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipo);
+        }
+
+        // POST: Equipoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Equipo equipo = db.Equipo.Find(id);
+            db.Equipo.Remove(equipo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/PrestamoController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PrestamoController : Controller
+    {
+        private bdVideoExplicativoEntitiesEntities db = new bdVideoExplicativoEntitiesEntities();
+
+        // GET: Prestamo
+        public ActionResult Index()
+        {
+            var prestamo = db.Prestamo.Include(p => p.Equipo).Include(p => p.Usuario);
+            return View(prestamo.ToList());
+        }
+
+        // GET: Prestamo/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prestamo prestamo = db.Prestamo.Find(id);
+            if (prestamo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prestamo);
+        }
+
+        // GET: Prestamo/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdEquipo = new SelectList(db.Equipo, "IdEquipo", "NombreEquipo");
+            ViewBag.IdUsuario = new SelectList(db.Usuario, "IdUsuario", "Usuario1");
+            return View();
+        }
+
+        // POST: Prestamo/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdPrestamo,FechaInicio,FechaFin,FechaEntrega,Multa,Estado,IdUsuario,IdEquipo")] Prestamo prestamo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Prestamo.Add(prestamo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdEquipo = new SelectList(db.Equipo, "IdEquipo", "NombreEquipo", prestamo.IdEquipo);
+            ViewBag.IdUsuario = new SelectList(db.Usuario, "IdUsuario", "Usuario1", prestamo.IdUsuario);
+            return View(prestamo);
+        }
+
+        // GET: Prestamo/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prestamo prestamo = db.Prestamo.Find(id);
+            if (prestamo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdEquipo = new SelectList(db.Equipo, "IdEquipo", "NombreEquipo", prestamo.IdEquipo);
+            ViewBag.IdUsuario = new SelectList(db.Usuario, "IdUsuario", "Usuario1", prestamo.IdUsuario);
+            return View(prestamo);
+        }
+
+        // POST: Prestamo/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdPrestamo,FechaInicio,FechaFin,FechaEntrega,Multa,Estado,IdUsuario,IdEquipo")] Prestamo prestamo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(prestamo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdEquipo = new SelectList(db.Equipo, "IdEquipo", "NombreEquipo", prestamo.IdEquipo);
+            ViewBag.IdUsuario = new SelectList(db.Usuario, "IdUsuario", "Usuario1", prestamo.IdUsuario);
+            return View(prestamo);
+        }
+
+        // GET: Prestamo/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prestamo prestamo = db.Prestamo.Find(id);
+            if (prestamo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prestamo);
+        }
+
+        // POST: Prestamo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Prestamo prestamo = db.Prestamo.Find(id);
+            db.Prestamo.Remove(prestamo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
